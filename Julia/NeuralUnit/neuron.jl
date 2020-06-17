38497c19db195d7909f5764eb0b164f83ef1561b
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2020-06-17 16:36:03 +0800

    y=2x^2+1

--- Julia/NeuralUnit/neuron.jl
+++ Julia/NeuralUnit/neuron.jl
@@ -1,32 +1,58 @@
-mutable struct Neuron{T<:Real}
-    w::Vector{T}
-    η::Real
-    b::T
-    ϕ::Function
-end
-Neuron(N,η,ϕ)=Neuron(rand(N),η,rand(),ϕ)
-
-sgn(x)=x>=0 ? 1 : -1
-
-function forward(n::Neuron{T},input::Vector{K}) where {T<:Real,K<:Real}
-    (n.w'*input+n.b)|>n.ϕ
+mutable struct Neuron{T <: Real}
+    w::Vector{T}    # 权重
+    η::Real         # 学习率
+    b::T            # 偏置
+    ϕ::Function     # 激活函数
 end
 
-function update!(n::Neuron{T},input::Vector{K},d::Real) where {T<:Real,K<:Real}
-    y=forward(n,input)
-    e=d-y
-    n.w=n.w+n.η*e*input
-    n.b=n.b+n.η*e
+Neuron(N,η,ϕ) = Neuron(rand(N), η, rand(), ϕ)
+
+function forward(n::Neuron{T}, input::Vector{K}) where {T <: Real,K <: Real}
+    (n.w' * input + n.b) |> n.ϕ
+end
+
+function update!(n::Neuron{T}, input::Vector{K}, d::Real) where {T <: Real,K <: Real}
+    y = forward(n, input)
+    e = d - y
+    n.w = n.w + n.η * e * input
+    n.b = n.b + n.η * e
     return e
 end
 
-data=[([x,y],x&y<1 ? -1 : 1) for x ∈ 0:1 for y ∈ 0:1]
 
-neuron=Neuron(2,0.03,sgn)
+sgn(x) = x >= 0 ? 1 : -1
+# 生成训练集
+data = [([x,y], x & y < 1 ? -1 : 1) for x ∈ 0:1 for y ∈ 0:1]
+# 定义神经元
+neuron = Neuron(2, 0.03, sgn)
+# 训练
 for epoch ∈ 1:10
-    for (x,y) in data
-        loss=update!(neuron,x,y)
+    for (x, y) in data
+        loss = update!(neuron, x, y)
         @info loss
         println(neuron)
+        s_x = s_y = range(-3, stop = 3, length = 100)
     end
 end
+
+
+# 拟合y=2*x^2+1
+using Plots
+using Random
+# 生成训练集
+X = -3:0.01:3 |> collect
+f(x,μ,σ) = 2x^2 + 1 + randn() * σ + μ
+f(x) = f(x, 0, 0.3)
+Y = f.(X)
+# 定义神经元
+func = Neuron(2, 0.001, identity)
+# 训练
+@gif for (x1, x2, y) in zip(X, X.^2, Y)
+    loss = update!(func, [x1,x2], y)
+    @info loss^2
+    plot(X, Y, label = "real")
+    plot!(X, x->func.w' * [x,x^2] + func.b, label = "predict")
+end
+# 拟合效果绘图
+plot(X,Y,label = "real")
+plot!(X,x->func.w' * [x,x^2] + func.b,label = "predict")
