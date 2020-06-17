0aac918d1df566dc86d8fae06df4880053ff6171
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2020-06-17 17:48:26 +0800

    used auto gradient

--- Julia/NeuralUnit/neuron.jl
+++ Julia/NeuralUnit/neuron.jl
@@ -1,3 +1,7 @@
+using Random 
+using Plots
+using Zygote
+
 mutable struct Neuron{T <: Real}
     w::Vector{T}    # 权重
     η::Real         # 学习率
@@ -8,28 +12,29 @@ end
 Neuron(N,η,ϕ) = Neuron(rand(N), η, rand(), ϕ)
 
 function forward(n::Neuron{T}, input::Vector{K}) where {T <: Real,K <: Real}
-    (n.w' * input + n.b) |> n.ϕ
+    σ = (n.w' * input + n.b) 
+    σ |> n.ϕ, gradient(n.ϕ,σ)[1]
 end
 
 function update!(n::Neuron{T}, input::Vector{K}, d::Real) where {T <: Real,K <: Real}
-    y = forward(n, input)
+    y, ∂y = forward(n, input)
     e = d - y
-    n.w = n.w + n.η * e * input
-    n.b = n.b + n.η * e
+    n.w = n.w + n.η * e * input .* ∂y
+    n.b = n.b + n.η * e * ∂y
     return e
 end
 
 
-sgn(x) = x >= 0 ? 1 : -1
 # 生成训练集
-data = [([x,y], x & y < 1 ? -1 : 1) for x ∈ 0:1 for y ∈ 0:1]
+data = [([x,y], x & y) for x ∈ 0:1 for y ∈ 0:1]
 # 定义神经元
-neuron = Neuron(2, 0.03, sgn)
+sigmoid(x)=1/(1+exp(-x))
+neuron = Neuron(2, 0.3, sigmoid)
 # 训练
-for epoch ∈ 1:10
+for epoch ∈ 1:100
     for (x, y) in data
         loss = update!(neuron, x, y)
-        @info loss
+        @info loss^2
         println(neuron)
     end
 end
@@ -47,11 +52,9 @@ Y = f.(X)
 func = Neuron(2, 0.001, identity)
 h(x) = func.w' * [x,x^2] + func.b
 # 训练
-@gif for (x1, x2, y) in zip(X, X.^2, Y)
+for (x1, x2, y) in zip(X, X.^2, Y)
     loss = update!(func, [x1,x2], y)
     @info loss^2
-    plot(X, Y, label = "real")
-    plot!(X, h, label = "predict")
 end
 # 拟合效果绘图
 plot(X,Y,label = "real")
